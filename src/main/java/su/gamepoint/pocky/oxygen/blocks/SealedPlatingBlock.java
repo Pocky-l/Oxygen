8da4fce11a96f3115ae189a76812987489927c89
Pocky <warface.erko@gmail.com>
2022-12-17 13:18:08 +0200

    sound type -> metal

--- src/main/java/su/gamepoint/pocky/oxygen/blocks/SealedPlatingBlock.java
+++ src/main/java/su/gamepoint/pocky/oxygen/blocks/SealedPlatingBlock.java
@@ -1,6 +1,7 @@
 package su.gamepoint.pocky.oxygen.blocks;
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 
@@ -9,6 +10,7 @@ public class SealedPlatingBlock extends Block {
     public SealedPlatingBlock() {
         super(BlockBehaviour.Properties.of(Material.METAL)
                 .explosionResistance(-1)
+                .sound(SoundType.METAL)
         );
     }
 }
