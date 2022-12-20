420324f582e33531ae01efb5a3beb666afffd0d6
Pocky <warface.erko@gmail.com>
2022-12-20 23:33:00 +0200

    Added colored sealed plating block

--- src/main/java/su/gamepoint/pocky/oxygen/registration/BlockRegister.java
+++ src/main/java/su/gamepoint/pocky/oxygen/registration/BlockRegister.java
@@ -12,6 +12,10 @@ import su.gamepoint.pocky.oxygen.blocks.SealedGlassBlock;
 import su.gamepoint.pocky.oxygen.blocks.SealedMembraneBlock;
 import su.gamepoint.pocky.oxygen.blocks.entities.block.OxygenControllerBlock;
 import su.gamepoint.pocky.oxygen.blocks.SealedPlatingBlock;
+import su.gamepoint.pocky.oxygen.utils.Color;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class BlockRegister {
 
@@ -21,6 +25,9 @@ public class BlockRegister {
     //blocks
     //public static final RegistryObject<Block> BLOCK_NAME = REGISTRY_BLOCK.register("block_id", BlockClass::new);
     public static final RegistryObject<Block> SEALED_PLATING = REGISTRY_BLOCK.register("sealed_plating", SealedPlatingBlock::new);
+
+    public static final Map<String, RegistryObject<Block>> COLORED_BLOCKS = createColoredBlocks();
+
     public static final RegistryObject<Block> SEALED_GLASS = REGISTRY_BLOCK.register("sealed_glass", SealedGlassBlock::new);
     public static final RegistryObject<Block> SEALED_MEMBRANE = REGISTRY_BLOCK.register("sealed_membrane", SealedMembraneBlock::new);
     public static final RegistryObject<Block> OXYGEN_CONTROLLER = REGISTRY_BLOCK.register("oxygen_controller", OxygenControllerBlock::new);
@@ -33,7 +40,30 @@ public class BlockRegister {
 
     public static final RegistryObject<Item> OXYGEN_CONTROLLER_ITEM = block(OXYGEN_CONTROLLER, OxygenMod.CREATIVE_MODE_TAB);
 
+    public static final Map<String, RegistryObject<Item>> COLORED_ITEMS = createColoredItems();
+
     private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
         return REGISTRY_ITEM.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
+
+    private static Map<String, RegistryObject<Block>> createColoredBlocks() {
+        var coloredBlocks = new HashMap<String, RegistryObject<Block>>();
+
+        for (Color color : Color.values()) {
+            coloredBlocks.put("sealed_plating_" + color.getColorName(),
+                    REGISTRY_BLOCK.register("sealed_plating_" + color.getColorName(), SealedPlatingBlock::new));
+        }
+
+        return coloredBlocks;
+    }
+
+    private static Map<String, RegistryObject<Item>> createColoredItems() {
+        var coloredItems = new HashMap<String, RegistryObject<Item>>();
+
+        COLORED_BLOCKS.forEach((k, e) -> {
+            coloredItems.put(k, block(e, OxygenMod.CREATIVE_MODE_TAB));
+        });
+
+        return coloredItems;
     }
 }
---
+++ src/main/java/su/gamepoint/pocky/oxygen/utils/Color.java
@@ -0,0 +1,31 @@
+package su.gamepoint.pocky.oxygen.utils;
+
+public enum Color {
+
+    WHITE("white"),
+    ORANGE("orange"),
+    MAGENTA("magenta"),
+    LIGHT_BLUE("light_blue"),
+    YELLOW("yellow"),
+    LIME("lime"),
+    PINK("pink"),
+    GRAY("gray"),
+    LIGHT_GRAY("light_gray"),
+    CYAN("cyan"),
+    PURPLE("purple"),
+    GREEN("green"),
+    BLUE("blue"),
+    BROWN("brown"),
+    RED("red"),
+    BLACK("black");
+
+    private final String colorName;
+
+    Color(String colorName) {
+        this.colorName = colorName;
+    }
+
+    public String getColorName() {
+        return colorName;
+    }
+}
